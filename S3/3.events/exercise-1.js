820ee5ea22caedc9a8d1a4f01cc000c4b6e11fad
gviil <guilbertmatos07@gmail.com>
2023-10-19 21:42:50 +0200

    S3 JS

---
+++ S3/3.events/exercise-1.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector(".btn");
+console.log(btn);
+btn.addEventListener("click" ,function (e){
+    console.log(e);
+
+});
---
+++ S3/3.events/exercise-3.js
@@ -0,0 +1,6 @@
+let input = document.querySelector("input");
+input.addEventListener("input", function (e){
+    
+    console.log(e.target.value);
+    
+});
